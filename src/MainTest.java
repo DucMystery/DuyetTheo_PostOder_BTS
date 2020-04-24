2030afbba97131f0aee314f741340de30072bcb8
DucMystery <58973644+DucMystery@users.noreply.github.com>
2020-04-24 09:08:11 +0700

    Hoan thanh PostOder BTS

---
+++ src/MainTest.java
@@ -0,0 +1,18 @@
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        TreeNode<Integer> tn = new TreeNode<>();
+
+        tn.insert(10);
+        tn.insert(18);
+        tn.insert(8);
+        tn.insert(7);
+        tn.insert(9);
+        tn.insert(11);
+        tn.insert(19);
+
+        tn.postOder();
+    }
+
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,45 @@
+public class TreeNode<E extends Comparable> {
+
+    Node<E> root;
+    public void insert(E element) {
+        if (root == null) {
+            root = new Node<>(element);
+        } else {
+            Node<E> currentNode = root;
+            while (currentNode != null) {
+                if (element.compareTo(currentNode.data) < 0) {
+                    if (currentNode.left == null) {
+                        currentNode.left = new Node<>(element);
+                        break;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else if (element.compareTo(currentNode.data) > 0) {
+                    if (currentNode.right == null) {
+                        currentNode.right = new Node<>(element);
+                        break;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                } else {
+                    throw new RuntimeException("Gia tri da ton tai !");
+                }
+
+            }
+
+
+        }
+    }
+    public void postOder(){
+        postOder(root);
+    }
+
+
+        void postOder(Node<E> root){
+        if (root ==null)return;
+        postOder(root.left);
+        postOder(root.right);
+        System.out.println(root.data);
+    }
+
+}
